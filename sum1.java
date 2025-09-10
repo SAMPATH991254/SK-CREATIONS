182467e5c3774c3956a262ee460944847ae4893d
SAMPATH991254 <sampath991254@gmail.com>
2025-09-10 10:11:23 +0530

    SK

---
+++ sum1.java
@@ -0,0 +1,8 @@
+public class sum1 {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 11;
+        int sum = a + b; 
+        System.out.println("sum: " + sum); 
+    }
+}
